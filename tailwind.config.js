da795a49ad43786e47867934fd582ed31819d75b
Fakhar-imran <fakharimran22@gmail.com>
2025-06-13 12:14:33 +0500

    upload totc project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'maincolor': '#00CBB8',
+        'secondrycolor':'#2F327D'
+      },
+    },
+  },
+  plugins: [],
+}
+

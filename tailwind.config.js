d695c7e91a78752b568020492dad80bb12b803f5
Fakhar-imran <fakharimran22@gmail.com>
2025-06-13 15:16:09 +0500

    update index

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         'maincolor': '#00CBB8',
         'secondrycolor':'#2F327D'
       },
+       fontFamily: {
+        'myfont':['Varela Round']
+      },
     },
   },
   plugins: [],
